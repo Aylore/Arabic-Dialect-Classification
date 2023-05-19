7e33b6ecf299dcebaa3c7411d25b1a6384f6bfb8
Osama Fayez <osamaoun997@gmail.com>
2023-05-19 09:15:39 +0300

    Deployment with dummy model

---
+++ deployment/app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import numpy as np
+
+
+app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post(  "/predict")
+async def predict(request: Request):
+    form_data = await request.form()
+    text = form_data["text"]
+
+    label = predict_label(text)
+    
+    return templates.TemplateResponse("prediction.html", {"request": request, "label": label})
+
+
+def predict_label(text):
+    # Dummy prediction logic
+    model_prediction = np.random.choice(["SD","LB","EG","MA", "LY"]) #Put model here
+    
+    dict_ = {"SD":"سودانية",
+             "LB":"ليبية",
+             "EG":"مصرية",
+             "MA":"مغربية",
+             "LY":"لبنانية"}
+    
+    return f"اللهجة: {dict_[model_prediction]}"
