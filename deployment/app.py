cfd76b26cd083c450a2bf28de37576294373ec80
Osama Fayez <osamaoun997@gmail.com>
2023-05-19 21:29:21 +0300

    confidence scores

--- deployment/app.py
+++ deployment/app.py
@@ -21,9 +21,22 @@ async def predict(request: Request):
     form_data = await request.form()
     text = form_data["text"]
 
-    label = predict_label(text)
-    
-    return templates.TemplateResponse("index.html", {"request": request, "label": label, "text": text})
+    label, SD_score, LB_score, EG_score, MA_score, LY_score = predict_label(text)
+    return templates.TemplateResponse("index.html",
+                                      {"request": request,
+                                       "label": label,
+                                       "text": text,
+                                       "SD_score" : "{:.1f}%".format(SD_score), 
+                                       "LB_score" : "{:.1f}%".format(LB_score), 
+                                       "EG_score" : "{:.1f}%".format(EG_score), 
+                                       "MA_score" : "{:.1f}%".format(MA_score), 
+                                       "LY_score" : "{:.1f}%".format(LY_score), 
+                                       "eg_width" : f"width: {EG_score}%;",
+                                       "lb_width" : f"width: {LB_score}%;",
+                                       "ly_width" : f"width: {LY_score}%;",
+                                       "ma_width" : f"width: {MA_score}%;",
+                                       "sd_width" : f"width: {SD_score}%;",
+                                       })
 
 @app.get("/clear/")
 async def clear(request: Request):
@@ -39,6 +52,12 @@ def predict_label(text):
              "MA":"moroccian - مغربية",
              "LY":"Lebanese -لبنانية"}
     
-    return dict_[model_prediction]
+    SD_score = np.random.rand() * 100
+    LB_score = np.random.rand() * 100
+    EG_score = np.random.rand() * 100
+    MA_score = np.random.rand() * 100
+    LY_score = np.random.rand()* 100
+
+    return dict_[model_prediction], SD_score, LB_score, EG_score, MA_score, LY_score
 
 # to run the app: uvicorn deployment.app:app --reload
